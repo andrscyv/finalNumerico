3800c77eca350efb294dbe498a313c424b90d249
Flandalanda <flandalanda@gmail.com>
2018-12-04 23:27:43 -0600

    El dos ya jala

--- ej2.m
+++ ej2.m
@@ -62,20 +62,27 @@ Vs = @(s) phi((log(s)+0.14)/sqrt(0.08))+dn((log(s)+0.14)/sqrt(0.08))/sqrt(0.08)-
 %VS numérica
 %Wx_anal = @(x) Vs(2.^x).*2.^x*log(2);
 %Calculamos Wx num�rica
-Wx_num(1) = 0;
+%Wx_num(1) = 0;
+%Wx_num(M(1/100)+1) = 2.8;
+
+Wx_num(1) = (-3*Crank(1,tFin)+4*Crank(2,tFin)-Crank(3,tFin))*50
+Wx_num(M(1/100)+1) = (-Crank(M(1/100)-1,tFin)+4*Crank(M(1/100),tFin)-3*Crank(M(1/100)+1,tFin))*-50;
+
 for i = 2:M(1/100)
-   Wx_num(i) = (Crank(i+1,tFin)-Crank(i-1,tFin))*5;
+   Wx_num(i) = (Crank(i+1,tFin)-Crank(i-1,tFin))*50;
 end
 
 solExact = Vex(linspace(0,4,M(1/100)+1));
 for i = 2:M(1/100)
-   Vs_anal(i) = (solExact(i+1)-solExact(i-1))*5;
+   Vs_anal(i) = (solExact(i+1)-solExact(i-1))*50;
 end
 
-Vs_anal(1)=0;
-Vs_anal(M(1/100)+1) = 1;
+%Vs_anal(1)=0;
+%Vs_anal(M(1/100)+1) = 1;
+Vs_anal(1) = (-3*solExact(1)+4*solExact(2)-solExact(2))*50
+Vs_anal(M(1/100)+1) = (-solExact(M(1/100)-1)+4*solExact(M(1/100))-3*solExact(M(1/100)+1))*-50;
 
-Wx_num(M(1/100)+1) = 2.8;
+
 Vs_num = @(Wx,x) Wx./(2.^(x)*log(2));
 Vs_res = Vs_num(Wx_num,gridSpace);
 close all;
@@ -87,11 +94,11 @@ plot(linspace(0,4,M(1/100)+1),Vs_anal)
 %Vxx analitica y numerica
 Wxx_num(1) = 0;
 for i = 2:M(1/100)
-   Wxx_num(i) = (Wx_num(i+1)-Wx_num(i-1))*5;
+   Wxx_num(i) = (Wx_num(i+1)-Wx_num(i-1))*50;
 end
 
 for i = 2:M(1/100)
-   Vss_anal(i) = (Vs_anal(i+1)-Vs_anal(i-1))*5;
+   Vss_anal(i) = (Vs_anal(i+1)-Vs_anal(i-1))*50;
 end
 
 Wxx_num(M(1/100)+1) = 2.8;
